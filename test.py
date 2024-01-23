fd5f9b703f3ad98a9b1c1359a442d96b005f7b09
Marcel Vosshans <marcel.vosshans@hs-esslingen.de>
2024-01-23 16:26:37 +0100

    removed cityscapes dataloader to StixelGen Project

--- test.py
+++
@@ -1,6 +0,0 @@
-from resultloader.cityscapes import CityscapesDataLoader
-
-
-city = CityscapesDataLoader(root_dir="C:\\Users\\marce\\Documents\\datasets\\cityscapes")
-data = city[101]
-print(len(data.gt_obstacles))
