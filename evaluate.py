97ad865ce75d230f337ca857d16191c1027b3f90
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-01-25 15:12:04 +0100

    updated for stereo

--- evaluate.py
+++ evaluate.py
@@ -3,41 +3,21 @@ import yaml
 with open('config.yaml') as yamlfile:
     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 import os
-import numpy as np
-from metrics.PrecisionRecall import PrecisionRecall, draw_stixel_on_image_prcurve
+from metrics.PrecisionRecall import PrecisionRecall
 from datetime import datetime
-if config['evaluation']['model'] == "Stereo":
-    from resultloader import StereoStixelLoader as Dataloader
-elif config['evaluation']['model'] == "StixelNExT":
-    from resultloader import StixelNExTLoader as Dataloader
+from resultloader import StereoStixelLoader as Dataloader
 
 overall_start_time = datetime.now()
 
 
 def main():
-    test_data_generator = Dataloader(exploring=config['explore_data'])
-    metric = PrecisionRecall(iou_threshold=config['evaluation']['iou'], rm_used=True)
-    if config['explore_data']:
-        test_data_generator.set_threshold(0.8)
-        pred, targ, image = test_data_generator[4]
-        precision, recall = metric.evaluate(pred, targ)
-        print(f"pred_len: {len(pred)}, targ_len: {len(targ)}")
-        print(f'Precision: {precision}, Recall: {recall}')
-        test_data_generator.stixel_reader.show_stixel(image, stixel_list=targ, color=[0, 255, 0])
-        test_data_generator.stixel_reader.show_stixel(image, stixel_list=pred, color=[255, 0, 0])
+    test_data_generator = Dataloader()
+    metric = PrecisionRecall(iou_threshold=config['evaluation']['iou'], rm_used=False)
     # Create an export of analysed data
     if config['logging']['activate']:
-        # Init the logger
-        # e.g. StixelNExT_ancient-silence-25_epoch-94_loss-0.09816327691078186.pth predictions_from_stereo
-        run = config['weights_file']
-        if config['evaluation']['model'] == "Stereo":
-            run = "Stereo"
-            checkpoint = "-"
-            epochs = "-"
-        else:
-            run_logger = run.split('_')[1]
-            checkpoint, _ = os.path.splitext(run)
-            epochs = run.split('_')[2].split('-')[1]
+        run_logger = "Stereo"
+        checkpoint = "-"
+        epochs = "-"
 
         wandb_logger = wandb.init(project=config['logging']['project'],
                                   config={
@@ -49,52 +29,34 @@ def main():
                                   tags=["metrics", "testing"]
                                   )
 
-        # overwrite of pred_threshold
-        thresholds = np.linspace(0.1, 1.0, num=config['evaluation']['num_thresholds'])
+        # A list with precision and recall per sample of testdata
+        precision_by_testdata = []
+        recall_by_testdata = []
+        f1_score_by_testdata = []
+        for sample in test_data_generator:
+            try:
+                prediction, target = sample
+            except FileNotFoundError as e:
+                continue
+            # iterate over all samples
+            precision, recall = metric.evaluate(prediction, target)
+            precision_by_testdata.append(precision)
+            recall_by_testdata.append(recall)
+            f1_score = metric.get_score()
+            f1_score_by_testdata.append(f1_score)
+        avg_prec = sum(precision_by_testdata) / len(precision_by_testdata)
+        avg_rec = sum(recall_by_testdata) / len(recall_by_testdata)
+        avg_f1 = sum(f1_score_by_testdata) / len(f1_score_by_testdata)
+        print(f"... Precision: {avg_prec}")
+        print(f"... Recall: {avg_rec}")
+        print(f"... F1 Score: {avg_f1}")
+        # calculate average over all samples and append to IoU list
+        step_time = datetime.now() - overall_start_time
+        print("Time elapsed: {}".format(step_time))
 
-        # A list with all average prec. and recall by IoU
-        precision_by_iou = []
-        recall_by_iou = []
-        f1_score_by_iou = []
-        for threshold in thresholds:
-            print(f"Evaluating threshold: {threshold} ...")
-            test_data_generator.set_threshold(threshold)
-            # A list with precision and recall per sample of testdata
-            precision_by_testdata = []
-            recall_by_testdata = []
-            f1_score_by_testdata = []
-            for sample in test_data_generator:
-                try:
-                    if config['explore_data']:
-                        prediction, target, _ = sample
-                    else:
-                        prediction, target = sample
-                except FileNotFoundError as e:
-                    continue
-                # iterate over all samples
-                precision, recall = metric.evaluate(prediction, target)
-                precision_by_testdata.append(precision)
-                recall_by_testdata.append(recall)
-                f1_score = metric.get_score()
-                f1_score_by_testdata.append(f1_score)
-            avg_prec = sum(precision_by_testdata) / len(precision_by_testdata)
-            avg_rec = sum(recall_by_testdata) / len(recall_by_testdata)
-            avg_f1 = sum(f1_score_by_testdata) / len(f1_score_by_testdata)
-            print(f"... Precision: {avg_prec}")
-            print(f"... Recall: {avg_rec}")
-            print(f"... F1 Score: {avg_f1}")
-            # calculate average over all samples and append to IoU list
-            precision_by_iou.append(avg_prec)
-            recall_by_iou.append(avg_rec)
-            f1_score_by_iou.append(avg_f1)
-            step_time = datetime.now() - overall_start_time
-            print("Time elapsed: {}".format(step_time))
-
-        data = [[x, y] for (x, y) in zip(recall_by_iou, precision_by_iou)]
-        table = wandb.Table(data=data, columns=["Recall", "Precision"])
-        wandb_logger.log({"PR curve": wandb.plot.line(table, "Recall", "Precision",
-                                                      title=f"Precision-Recall over {len(test_data_generator)} samples")})
-        wandb_logger.log({"F1": sum(f1_score_by_iou) / len(f1_score_by_iou)})
+        wandb_logger.log({"Precision": avg_prec})
+        wandb_logger.log({"Recall": avg_rec})
+        wandb_logger.log({"F1": avg_f1})
 
 
 if __name__ == '__main__':
--- resultloader/MultiStixelStereo.py
+++ resultloader/MultiStixelStereo.py
@@ -7,11 +7,21 @@ with open('config.yaml') as yamlfile:
 
 class StereoStixelLoader(EvaluationDataloader):
     def __init__(self):
-        self.prediction_folder = os.path.join(config['data_path'], config['dataset'], 'testing', 'calculations_from_Stereo')
+        self.prediction_folder = os.path.join(config['data_path'], config['dataset'], 'testing', 'calculations_from_stereo')
         super().__init__(os.path.join(os.path.dirname(self.prediction_folder), "targets_from_lidar"))
         self.prediction_list = [pred for pred in os.listdir(self.prediction_folder) if pred.endswith(".csv")]
         if len(self.prediction_list) != len(self.target_list):
-            print(f"INFO: Inconsistent number of predictions[{len(self.prediction_list)}] and targets[{len(self.target_list)}]")
+            print(f"INFO: Inconsistent number of predictions[{len(self.prediction_list)}] and targets[{len(self.target_list)}]:")
+            print("In target but not in predictions")
+            for name in self.target_list:
+                if name not in self.prediction_list:
+                    print(name)
+            print("In prediction but not in targets")
+            for name in self.prediction_list:
+                if name not in self.target_list:
+                    print(name)
+        else:
+            print(f"Found {len(self.prediction_list)} samples.")
 
     def __getitem__(self, idx):
         filename = self.prediction_list[idx]
