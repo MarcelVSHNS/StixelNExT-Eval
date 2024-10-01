9aeca45a656411daaf221c55bc0a3293e402fb6b
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-10-01 10:43:27 +0200

    version 0.98.1

--- analyse.py
+++ analyse.py
@@ -1,17 +1,19 @@
 import multiprocessing
 import os
 import os.path
+from collections import defaultdict
 from datetime import datetime
 from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
+import stixel as stx
 import yaml
+from stixel.utils.packing import add_image
 
 import wandb
 from dataloader import WaymoDataLoader, StixelModel
 from metric import evaluate_sample_3dbbox
-from utils import draw_stixel_and_bboxes
 
 with open('config.yaml') as yaml_file:
     config = yaml.load(yaml_file, Loader=yaml.FullLoader)
@@ -33,7 +35,7 @@ def main():
     result_dir = os.path.join('sample_results', stxl_model.checkpoint_name)
     os.makedirs(result_dir, exist_ok=True)
 
-    index_list = list(range(0, len(loader), 8))
+    index_list = list(range(0, len(loader), 200))
     analyse_partial = partial(analyse,
                               stxl_model=stxl_model,
                               dataloader=loader
@@ -43,16 +45,30 @@ def main():
 
     # save every precision/ recall of every sample and probability
     # e.g. a dict with probability as key and a list of precisions (by sample) as value
-    precision = {}
-    recall = {}
+    precision = defaultdict(list)
+    recall = defaultdict(list)
+    num_stx = defaultdict(list)
     for sample in results:
         for probab, result in sample['results'].items():
             precision[probab].append(result['precision'])
             recall[probab].append(result['recall'])
+            num_stx[probab].append(len(result['stxl_wrld'].stixel))
+
+    avg_precision = {probab: sum(vals) / len(vals) for probab, vals in precision.items()}
+    avg_recall = {probab: sum(vals) / len(vals) for probab, vals in recall.items()}
+    avg_num_stx = {probab: sum(vals) / len(vals) for probab, vals in num_stx.items()}
+
+    for probab in avg_precision:
+        wandb.log({
+            "Precision": avg_precision[probab],
+            "Recall": avg_recall[probab],
+            "Num Stixel": avg_num_stx[probab],
+            "Probability": probab
+        })
 
     # draw precision recall curve
     plt.figure()
-    plt.plot(recall, precision, label='NN', marker='o', color='turquoise')
+    plt.plot(avg_recall, avg_precision, label='NN', marker='o', color='turquoise')
     plt.plot(1.155, 0.974, label='GT', marker='x', color='fuchsia')
     plt.xlabel('Recall')
     plt.ylabel('Precision')
@@ -61,26 +77,24 @@ def main():
     plt.legend()
     plt.savefig(os.path.join(result_dir, name + '.png'))
 
-    # draw best in 3d
-    best_prob = None
-    max_sum = -1
-    for prob, metrics in probab_result.items():
-        precision = metrics['precision']
-        recall = metrics['recall']
-
-        if precision + recall > 0:
-            f1_score = 2 * (precision * recall) / (precision + recall)
-        else:
-            f1_score = 0
-
-        if f1_score > max_f1:
-            max_f1 = f1_score
-            best_prob = prob
-    print(
-        f"Probability: {best_prob} with prec: {probab_result[best_prob]['precision']} with recall: {probab_result[best_prob]['recall']}")
-    draw_stixel_and_bboxes(probab_result[best_prob]['pts'], probab_result[best_prob]['colors'], sample.bboxes)
-
+    sample = results[0]
+    for probab, result in sample['results'].items():
+        image = result['image']
+        stxl_wrld = result['stxl_wrld']
+        stxl_wrld = add_image(stxl_wrld, image)
+        stxl_img = stx.draw_stixels_on_image(stxl_wrld)
+        wandb.log({f"Sample @ {probab}": wandb.Image(stxl_img)})
+    best_f1 = {"f1_score": 0.0}
+    for probab, result in sample['results'].items():
+        if result['f1_score'] > best_f1["f1_score"]:
+            best_f1["image"] = result['image']
+            best_f1["stxl_wrld"] = result['stxl_wrld']
     logger.finish()
+    # show best in 3d and 2d
+    stxl_wrld = add_image(best_f1["stxl_wrld"], best_f1["image"])
+    stxl_img = stx.draw_stixels_on_image(stxl_wrld)
+    stxl_img.show()
+    stx.draw_stixels_in_3d(stxl_wrld)
 
 
 def analyse(sample_idx: float, stxl_model: StixelModel, dataloader: WaymoDataLoader):
@@ -91,8 +105,8 @@ def analyse(sample_idx: float, stxl_model: StixelModel, dataloader: WaymoDataLoa
         # Inference a Stixel World
         start_inf = datetime.now()
         stxl_wrld = stxl_model.inference(sample.image, probability=probability, calib=sample.calib)
-        if len(stxl_wrld.stixel) > 2000:
-            continue
+        # if len(stxl_wrld.stixel) > 2000:
+        #     continue
         print(f"Inference: {datetime.now() - start_inf}")
         # Apply the evaluation
         start_eval = datetime.now()
